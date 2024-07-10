80ca1dc127a71329793edbbcf53dd122136d9ccf
Janosch Gräf <janosch.graef@gmail.com>
2024-07-10 08:40:03 +0200

    documentation

---
+++ README.md
@@ -0,0 +1,19 @@
+# `semver-macro`
+
+[![crates.io](https://img.shields.io/crates/v/semver-macro.svg)](https://crates.io/crates/semver-macro)
+[![Documentation](https://docs.rs/semver-macro/badge.svg)](https://docs.rs/semver-macro)
+[![MIT License](https://img.shields.io/crates/l/semver-macro.svg)](./LICENSE)
+
+This crate contains a single proc-macro to generate [`semver::Version`][1] at compile-time.
+Currently only versions without pre-release and build metadata identifier are supported.
+
+## Example
+
+```rust
+use semver::Version;
+use semver_macro::version;
+
+const MY_VERSION: Version = version!("0.1.0");
+```
+
+[1]: https://docs.rs/semver/latest/semver/struct.Version.html
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,33 @@
+//! A proc-macro for [`semver::Version`]
+//!
+//! This crate contains a single proc-macro to generate [`semver::Version`]
+//! at compile-time. Currently only versions without pre-release and build
+//! metadata identifier are supported.
+//!
+//! # Example
+//!
+//! ```
+//! use semver::Version;
+//! use semver_macro::version;
+//!
+//! const MY_VERSION: Version = version!("0.1.0");
+//! ```
+
 use proc_macro_error::{abort, proc_macro_error};
 use quote::quote_spanned;
 use semver::Version;
 use syn::{parse_macro_input, LitStr};
 
+/// Proc-macro that parses a [`semver::Version`] from string and outputs it as a constant expression.
+///
+/// # Example
+///
+/// ```
+/// use semver::Version;
+/// use semver_macro::version;
+///
+/// const MY_VERSION: Version = version!("0.1.0");
+/// ```
 #[proc_macro_error]
 #[proc_macro]
 pub fn version(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
